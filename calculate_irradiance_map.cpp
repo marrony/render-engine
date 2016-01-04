edc7fe15f70d073d294a79ab505a1e9ba84ab211
Marrony Neris <neris.marrony@gmail.com>
2016-01-04 17:22:52 -0200

    Pre-calculate prefilterEnvMap and integrateBRDF and save into textures

--- calculate_irradiance_map.cpp
+++ calculate_irradiance_map.cpp
@@ -12,6 +12,7 @@
 
 #include <chrono>
 #include <thread>
+#include <algorithm>
 
 #include "Vector.h"
 #include "Matrix.h"
@@ -19,54 +20,49 @@
 #include "Allocator.h"
 #include "TextureManager.h"
 
-void imageGet3(Image& image, int x, int y, uint8_t pixels[3]) {
+#define IMG_PATH "images/LancellottiChapel"
+#define NUM_SAMPLES 128
+#define OUTPUT_SIZE 128
+
+void imageGet3(const Image& image, int x, int y, float pixels[3]) {
     int offsety = y*image.format*image.width;
     int offsetx = x*image.format;
 
-    pixels[0] = image.pixels[offsety + offsetx + 0];
-    pixels[1] = image.pixels[offsety + offsetx + 1];
-    pixels[2] = image.pixels[offsety + offsetx + 2];
+    pixels[0] = image.pixels[offsety + offsetx + 0] / 255.0f;
+    pixels[1] = image.pixels[offsety + offsetx + 1] / 255.0f;
+    pixels[2] = image.pixels[offsety + offsetx + 2] / 255.0f;
 }
 
-void imageSet3(Image& image, int x, int y, uint8_t pixels[3]) {
+void imageSet3(Image& image, int x, int y, float pixels[3]) {
     int offsety = y*image.format*image.width;
     int offsetx = x*image.format;
 
-    image.pixels[offsety + offsetx + 0] = pixels[0];
-    image.pixels[offsety + offsetx + 1] = pixels[1];
-    image.pixels[offsety + offsetx + 2] = pixels[2];
+    image.pixels[offsety + offsetx + 0] = pixels[0] * 255.0f;
+    image.pixels[offsety + offsetx + 1] = pixels[1] * 255.0f;
+    image.pixels[offsety + offsetx + 2] = pixels[2] * 255.0f;
 }
 
-void imageSet3(Image& image, int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-    uint8_t pixels[3] = {r, g, b};
+void imageSet3(Image& image, int x, int y, float r, float g, float b) {
+    float pixels[3] = {r, g, b};
     imageSet3(image, x, y, pixels);
 }
 
-void sampleFace(int face, Image input[6], float sc, float tc, float ma, float color[3]) {
+void sampleFace(const Image& input, float sc, float tc, float ma, float color[3]) {
     ma = fabs(ma);
 
     float s = (sc/ma + 1) * 0.5;
     float t = (tc/ma + 1) * 0.5;
 
-//    printf("[%f %f] %f [%f %f]\n", sc, tc, ma, s, t);
+    int x = s * (input.width - 1);
+    int y = t * (input.height - 1);
 
-    int x = s * (input[face].width - 1);
-    int y = t * (input[face].height - 1);
+    assert(x < input.width);
+    assert(y < input.height);
 
-    assert(x < input[face].width);
-    assert(y < input[face].height);
-
-    uint8_t pixels[3];
-    imageGet3(input[face], x, y, pixels);
-
-    //printf("%d [%d %d] [%d %d %d] [%d %d %d]\n", face, xx, yy, r, g, b, pixels[r], pixels[1], pixels[2]);
-
-    color[0] = float(pixels[0]) / 255.0f;
-    color[1] = float(pixels[1]) / 255.0f;
-    color[2] = float(pixels[2]) / 255.0f;
+    imageGet3(input, x, y, color);
 }
 
-void sampleCube(Image input[6], float vector[3], float color[3]) {
+void sampleCube(const ImageCube& input, float vector[3], float color[3]) {
     float x = fabs(vector[0]);
     float y = fabs(vector[1]);
     float z = fabs(vector[2]);
@@ -78,9 +74,9 @@ void sampleCube(Image input[6], float vector[3], float color[3]) {
 
     if(x >= y && x >= z) {
         if(vector[0] >= 0)
-            sampleFace(POSITIVE_X, input, -vector[2], +vector[1], vector[0], temp);
+            sampleFace(input.faces[POSITIVE_X], -vector[2], +vector[1], vector[0], temp);
         else
-            sampleFace(NEGATIVE_X, input, +vector[2], +vector[1], vector[0], temp);
+            sampleFace(input.faces[NEGATIVE_X], +vector[2], +vector[1], vector[0], temp);
 
         mnVector3Add(color, temp, color);
         count += 1;
@@ -88,9 +84,9 @@ void sampleCube(Image input[6], float vector[3], float color[3]) {
 
     if(y >= x && y >= z) {
         if(vector[1] >= 0)
-            sampleFace(POSITIVE_Y, input, +vector[0], -vector[2], vector[1], temp);
+            sampleFace(input.faces[POSITIVE_Y], +vector[0], -vector[2], vector[1], temp);
         else
-            sampleFace(NEGATIVE_Y, input, +vector[0], +vector[2], vector[1], temp);
+            sampleFace(input.faces[NEGATIVE_Y], +vector[0], +vector[2], vector[1], temp);
 
         mnVector3Add(color, temp, color);
         count += 1;
@@ -98,9 +94,9 @@ void sampleCube(Image input[6], float vector[3], float color[3]) {
 
     if(z >= x && z >= y) {
         if(vector[2] >= 0)
-            sampleFace(POSITIVE_Z, input, +vector[0], +vector[1], vector[2], temp);
+            sampleFace(input.faces[POSITIVE_Z], +vector[0], +vector[1], vector[2], temp);
         else
-            sampleFace(NEGATIVE_Z, input, -vector[0], +vector[1], vector[2], temp);
+            sampleFace(input.faces[NEGATIVE_Z], -vector[0], +vector[1], vector[2], temp);
 
         mnVector3Add(color, temp, color);
         count += 1;
@@ -152,27 +148,6 @@ void hammersley2D( uint32_t i, uint32_t N, float out[2] ) {
     out[1] = E2;
 }
 
-void randomizeLinear(uint32_t i, uint32_t N, float out[2]) {
-    float s = sqrtf(float(N));
-
-    float x = float(i) / s;
-    float y = fmodf(i, s);
-
-    out[0] = x / s;
-    out[1] = y / s;
-}
-
-void generateRandom(uint32_t i, uint32_t N, float out[2]) {
-    out[0] = (rand() % N) / (float)N;
-    out[1] = (rand() % N) / (float)N;
-}
-
-void generateSamplePoint(uint32_t i, uint32_t N, float out[2]) {
-    hammersley2D(i, N, out);
-//    randomizeLinear(i, N, out);
-//    generateRandom(i, N, out);
-}
-
 float* uniformSampleHemisphere( float E[2], float Out[4] ) {
     float Phi = 2 * M_PI * E[0];
     float CosTheta = E[1];
@@ -199,87 +174,80 @@ float* cosineSampleHemisphere( float E[2], float Out[4] ) {
     return Out;
 }
 
-void computePixelIrradiance(int face, Image input[6], float s, float t, float irradiance[3]) {
-    float normal[3];
+void normalForFace(int face, int x, int y, int width, int height, float N[3]) {
+    float s = x / (float)(width-1);
+    float t = y / (float)(height-1);
 
-    float x = s * 2 - 1;
-    float y = t * 2 - 1;
+    float xx = s * 2 - 1;
+    float yy = t * 2 - 1;
 
     if(face == POSITIVE_Y) {
-        float temp[3] = {x,  1, -y};
-        mnVector3Normalize(temp, normal);
+        float temp[3] = {xx,  1, -yy};
+        mnVector3Normalize(temp, N);
     } else if(face == NEGATIVE_Y) {
-        float temp[3] = {x, -1,  y};
-        mnVector3Normalize(temp, normal);
+        float temp[3] = {xx, -1,  yy};
+        mnVector3Normalize(temp, N);
     } else if(face == POSITIVE_X) {
-        float temp[3] = {1, y, -x};
-        mnVector3Normalize(temp, normal);
+        float temp[3] = {1, yy, -xx};
+        mnVector3Normalize(temp, N);
     } else if(face == NEGATIVE_X) {
-        float temp[3] = {-1, y, x};
-        mnVector3Normalize(temp, normal);
+        float temp[3] = {-1, yy, xx};
+        mnVector3Normalize(temp, N);
     } else if(face == POSITIVE_Z) {
-        float temp[3] = {x, y, 1};
-        mnVector3Normalize(temp, normal);
+        float temp[3] = {xx, yy, 1};
+        mnVector3Normalize(temp, N);
     } else if(face == NEGATIVE_Z) {
-        float temp[3] = {-x, y, -1};
-        mnVector3Normalize(temp, normal);
+        float temp[3] = {-xx, yy, -1};
+        mnVector3Normalize(temp, N);
     }
+}
 
+void computeIrradiance(int face, const ImageCube& input, const float N[3], float irradiance[3]) {
     float sampledColour[3] = {0, 0, 0};
 
-    int NumSamples = 128;
-    for(int i = 0; i < NumSamples; i++) {
+    for(int i = 0; i < NUM_SAMPLES; i++) {
         float Xi[2];
         float L[4];
         float H[3];
 
-        generateSamplePoint(i, NumSamples, Xi);
+        hammersley2D(i, NUM_SAMPLES, Xi);
         cosineSampleHemisphere(Xi, L);
-//        uniformSampleHemisphere(Xi, L);
-        tangentToWorld(L, normal, L);
+        tangentToWorld(L, N, L);
 
-#if 0
-        mnVector3Add(L, normal, L);
-        mnVector3Normalize(L, L);
-#endif
-
-        float NdotL = mnVector3Dot(normal, L);
+        float NdotL = mnVector3Dot(N, L);
         if(NdotL > 0) {
             float Li[3];
 
             sampleCube(input, L, Li);
 
             mnVector3Add(sampledColour, Li, sampledColour);
-//            mnVector3MulAddScalar(Li, NdotL, sampledColour);
         }
     }
 
-    mnVector3MulScalar(sampledColour, 1 / (float)NumSamples, irradiance);
+    mnVector3MulScalar(sampledColour, 1 / (float)NUM_SAMPLES, irradiance);
 }
 
-void computeFaceIrradiance(int face, Image input[6], int width, int height, Image& output) {
-    for(int y = 0; y < height; y++) {
-        for(int x = 0; x < width; x++) {
-            float irradiance[3];
+void computeFaceIrradiance(int face, const ImageCube& input, Image& output) {
+    for(int y = 0; y < output.height; y++) {
+        for(int x = 0; x < output.width; x++) {
+            float N[3];
+            float outColor[3];
 
-            float s = x / (float)(width-1);
-            float t = y / (float)(height-1);
-            computePixelIrradiance(face, input, s, t, irradiance);
+            normalForFace(face, x, y, output.width, output.height, N);
+            computeIrradiance(face, input, N, outColor);
 
-            //printf("%d %d %d %f %f %f\n", i, j, img, irradiance[0], irradiance[1], irradiance[2]);
-
-            imageSet3(output, x, y, irradiance[0]*255, irradiance[1]*255, irradiance[2]*255);
+            imageSet3(output, x, y, outColor);
         }
     }
 }
 
-void computeIrradiance(HeapAllocator& allocator, Image input[6], int width, int height, Image output[6]) {
+void computeDiffuseIrradiance(HeapAllocator& allocator, const ImageCube& input, int width, int height, ImageCube& output) {
     for(int face = 0; face < 6; face++) {
-        output[face].width = width;
-        output[face].height = height;
-        output[face].format = input[face].format;
-        output[face].pixels = (uint8_t*)allocator.allocate(3 * width * height);
-        memset(output[face].pixels, 0, 3 * width * height);
+        output.faces[face].width = width;
+        output.faces[face].height = height;
+        output.faces[face].format = input.faces[face].format;
+        output.faces[face].pixels = (uint8_t*)allocator.allocate(3 * width * height);
+        memset(output.faces[face].pixels, 0, 3 * width * height);
     }
 
     auto start = std::chrono::high_resolution_clock::now();
@@ -290,7 +258,7 @@ void computeIrradiance(HeapAllocator& allocator, Image input[6], int width, int 
 
         auto start = std::chrono::high_resolution_clock::now();
 
-        computeFaceIrradiance(face, input, width, height, output[face]);
+        computeFaceIrradiance(face, input, output.faces[face]);
 
         auto end = std::chrono::high_resolution_clock::now();
 
@@ -303,7 +271,7 @@ void computeIrradiance(HeapAllocator& allocator, Image input[6], int width, int 
 
     for(int face = 0; face < 6; face++) {
         tr[face] = std::thread([=]() {
-            computeFaceIrradiance(face, input, width, height, output[face]);
+            computeFaceIrradiance(face, input, output[face]);
         });
     }
 
@@ -316,50 +284,226 @@ void computeIrradiance(HeapAllocator& allocator, Image input[6], int width, int 
     printf("Total time: %lld millis\n", milli);
 }
 
+void ImportanceSampleGGX(const float Xi[2], float roughness, float H[3]) {
+    float a = roughness * roughness;
+
+    float phi = 2 * M_PI * Xi[0];
+    float cosTheta = sqrtf( (1 - Xi[1]) / (1 + (a*a - 1) * Xi[1]) );
+    float sinTheta = sqrtf( 1 - cosTheta * cosTheta );
+
+    H[0] = sinTheta * cosf(phi);
+    H[1] = sinTheta * sinf(phi);
+    H[2] = cosTheta;
+}
+
+//I - 2.0 * dot ( N , I ) * N
+void reflect(float k, const float I[3], const float N[3], float out[3]) {
+    float Ii[3];
+
+    mnVector3MulScalar(I, k, Ii);
+
+    float dot = 2.0f * mnVector3Dot(N, Ii);
+
+    mnVector3MulScalar(N, dot, out);
+    mnVector3Sub(Ii, out, out);
+}
+
+////input world space [N = V = R]
+void prefilterEnvMap(const ImageCube& input, float roughness, const float R[3], float out[3]) {
+    float prefilterColor[3] = {0, 0, 0};
+    float totalWeight = 0;
+
+    for(uint32_t i = 0; i < NUM_SAMPLES; i++) {
+        float Xi[2];
+        float H[3];
+        float L[3];
+
+        hammersley2D(i, NUM_SAMPLES, Xi);
+        ImportanceSampleGGX(Xi, roughness, H);
+        tangentToWorld(H, R, H);
+        reflect(-1, R, H, L);
+
+        float NdotL = std::max(0.0f, mnVector3Dot(R, L));
+        if (NdotL > 0) {
+            float Li[3];
+
+            sampleCube(input, L, Li);
+
+            mnVector3MulAddScalar(Li, NdotL, prefilterColor);
+            totalWeight += NdotL;
+        }
+    }
+
+    mnVector3MulScalar(prefilterColor, 1.0f / std::max(totalWeight, 0.001f), out);
+}
+
+void computePrefilterEnvMapForFace(int face, const ImageCube& input, float roughness, Image& output) {
+    for(int y = 0; y < output.height; y++) {
+        for(int x = 0; x < output.width; x++) {
+            float N[3];
+            float outColor[3];
+
+            normalForFace(face, x, y, output.width, output.height, N);
+            prefilterEnvMap(input, roughness, N, outColor);
+
+            imageSet3(output, x, y, outColor);
+        }
+    }
+}
+
+void computePrefilterEnvMap(HeapAllocator& allocator, const ImageCube& input, float roughness, int width, int height, ImageCube& output) {
+    for(int face = 0; face < 6; face++) {
+        output.faces[face].width = width;
+        output.faces[face].height = height;
+        output.faces[face].format = input.faces[face].format;
+        output.faces[face].pixels = (uint8_t*)allocator.allocate(3 * width * height);
+        memset(output.faces[face].pixels, 0, 3 * width * height);
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for(int face = 0; face < 6; face++) {
+        computePrefilterEnvMapForFace(face, input, roughness, output.faces[face]);
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    long long milli = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    printf("Total time: %lld millis\n", milli);
+}
+
+float G1_smithGGX(float alpha, float NdotX) {
+    float NdotX2 = NdotX * NdotX;
+    float alpha2 = alpha * alpha;
+
+    return NdotX + sqrtf(NdotX2 * (1 - alpha2) + alpha2);
+}
+
+float GVis_smithGGX(float roughness, float NdotV, float NdotL) {
+    float alpha = roughness*roughness;
+
+    return 1 / (G1_smithGGX(alpha, NdotL) * G1_smithGGX(alpha, NdotV));
+}
+
+void integrateBRDF(float roughness, float NdotV, float out[2]) {
+    float V[3];
+    V[0] = sqrtf(1 - NdotV * NdotV);
+    V[1] = 0;
+    V[2] = NdotV;
+
+    float A = 0;
+    float B = 0;
+
+    for(int i = 0; i < NUM_SAMPLES; i++) {
+        float Xi[2];
+        float H[3];
+        float L[3];
+
+        hammersley2D(i, NUM_SAMPLES, Xi);
+        ImportanceSampleGGX(Xi, roughness, H);
+        reflect(-1, V, H, L);
+
+        float NdotL = std::max(0.0f, L[2]);
+        float NdotH = std::max(0.0f, H[2]);
+        float VdotH = std::max(0.0f, mnVector3Dot(V, H));
+
+        if(NdotL > 0) {
+            //f = DFG / (4*NdotV*NdotL)
+
+            float G_Vis = GVis_smithGGX(roughness, NdotV, NdotL);
+            float NdotL_G_Vis_Pdf = NdotL * G_Vis * (4 * VdotH / NdotH);
+
+            float Fc = pow(1 - VdotH, 5);
+            A += (1 - Fc) * NdotL_G_Vis_Pdf;
+            B += Fc * NdotL_G_Vis_Pdf;
+        }
+    }
+
+    out[0] = A / (float)NUM_SAMPLES;
+    out[1] = B / (float)NUM_SAMPLES;
+}
+
+void calculateIntegrateBRDF(HeapAllocator& allocator, int width, int height, Image& output) {
+    output.width = width;
+    output.height = height;
+    output.format = 3;
+    output.pixels = (uint8_t*)allocator.allocate(3 * width * height);
+    memset(output.pixels, 0, 3 * width * height);
+
+    for(int x = 0; x < width; x++) {
+        float roughness = x / float(width - 1);
+
+        for(int y = 0; y < height; y++) {
+            float outColor[3] = {0, 0, 0};
+
+            float NdotV = y / float(height - 1);
+
+            integrateBRDF(roughness, NdotV, outColor);
+
+            imageSet3(output, x, y, outColor);
+        }
+    }
+}
+
 int main() {
     srand(time(nullptr));
 
     HeapAllocator heapAllocator;
 
-    Image images[6];
-    Image output[6];
+    ImageCube input;
+    readJpeg(heapAllocator, IMG_PATH"/posx.jpg", input.faces[POSITIVE_X]);
+    readJpeg(heapAllocator, IMG_PATH"/negx.jpg", input.faces[NEGATIVE_X]);
+    readJpeg(heapAllocator, IMG_PATH"/posy.jpg", input.faces[POSITIVE_Y]);
+    readJpeg(heapAllocator, IMG_PATH"/negy.jpg", input.faces[NEGATIVE_Y]);
+    readJpeg(heapAllocator, IMG_PATH"/posz.jpg", input.faces[POSITIVE_Z]);
+    readJpeg(heapAllocator, IMG_PATH"/negz.jpg", input.faces[NEGATIVE_Z]);
 
-#define IMG_PATH "images/LancellottiChapel"
+#if 1
+    {
+        ImageCube output;
+        computeDiffuseIrradiance(heapAllocator, input, OUTPUT_SIZE, OUTPUT_SIZE, output);
+        saveCube(IMG_PATH"/diffuse_irradiance.irr", output);
 
-    readJpeg(heapAllocator, IMG_PATH"/posx.jpg", images[POSITIVE_X]);
-    readJpeg(heapAllocator, IMG_PATH"/negx.jpg", images[NEGATIVE_X]);
-    readJpeg(heapAllocator, IMG_PATH"/posy.jpg", images[POSITIVE_Y]);
-    readJpeg(heapAllocator, IMG_PATH"/negy.jpg", images[NEGATIVE_Y]);
-    readJpeg(heapAllocator, IMG_PATH"/posz.jpg", images[POSITIVE_Z]);
-    readJpeg(heapAllocator, IMG_PATH"/negz.jpg", images[NEGATIVE_Z]);
-
-//    for(int i = 0; i < 6; i++) {
-//        int byte = 0;
-//
-//        if(i == POSITIVE_Y)
-//            byte = 0xff;
-//
-//        int size = images[i].format*images[i].width*images[i].height;
-//        memset(images[i].pixels, byte, size);
-//    }
-
-    computeIrradiance(heapAllocator, images, 128, 128, output);
-
-    const char* filename[] = {
-        IMG_PATH"/irradiance_posx.irr",
-        IMG_PATH"/irradiance_negx.irr",
-        IMG_PATH"/irradiance_posy.irr",
-        IMG_PATH"/irradiance_negy.irr",
-        IMG_PATH"/irradiance_posz.irr",
-        IMG_PATH"/irradiance_negz.irr",
-    };
-
-    for(int i = 0; i < 6; i++) {
-        saveImage(filename[i], output[i]);
-
-        heapAllocator.deallocate(output[i].pixels);
-        heapAllocator.deallocate(images[i].pixels);
+        for(int i = 0; i < 6; i++)
+            heapAllocator.deallocate(output.faces[i].pixels);
     }
+#endif
+
+#if 1
+    int size = OUTPUT_SIZE;
+    float maxMips = log2f((float)size);
+
+    for(float mip = 0; mip <= maxMips; mip += 1) {
+        char filename[1024];
+        snprintf(filename, 1024, "%s/prefilter_env_map_%d.irr", IMG_PATH, (int)mip);
+
+        ImageCube output;
+
+        float roughness = mip / maxMips;
+
+        printf("Calculating prefilterEnvMap(roughness=%.2f, width=%d, height=%d)\n", roughness, size, size);
+
+        computePrefilterEnvMap(heapAllocator, input, roughness, size, size, output);
+
+        saveCube(filename, output);
+
+        for(int i = 0; i < 6; i++)
+            heapAllocator.deallocate(output.faces[i].pixels);
+
+        size >>= 1;
+    }
+#endif
+
+#if 1
+    {
+        Image output;
+        calculateIntegrateBRDF(heapAllocator, OUTPUT_SIZE, OUTPUT_SIZE, output);
+        saveImage(IMG_PATH"/integrate_brdf.irr", output);
+        heapAllocator.deallocate(output.pixels);
+    }
+#endif
+
+    for(int i = 0; i < 6; i++)
+        heapAllocator.deallocate(input.faces[i].pixels);
 
     return 0;
 }
